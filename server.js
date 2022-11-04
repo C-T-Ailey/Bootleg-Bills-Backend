2bd8e94a6627ae4ef653a22f438cc9a9071c5884
Tom Biscuit <chris.ailey90@gmail.com>
2022-11-04 02:36:32 +0000

    implemented routes for about.js

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ app.use(express.json())
 const PORT = process.env.PORT;
 
 const indexRouter = require("./routes/index")
+const aboutRouter = require("./routes/about")
 const productCtrl = require("./routes/product")
 const authRouter = require("./routes/auth")
 const cartRouter = require("./routes/cart")
 const newsLetterRouter = require("./routes/newsletter")
 
 app.use("/", indexRouter);
+app.use("/", aboutRouter);
 app.use("/", productCtrl)
 app.use("/", authRouter);
 app.use("/", cartRouter);
